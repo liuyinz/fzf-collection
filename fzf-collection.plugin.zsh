11e0a0f6820621672192775d05d08c5dfe88cc3b
liuyinz <cheunghsu@outlook.com>
2021-04-29 18:39:04 +0800

    refactor: for loop source

--- fzf-collection.plugin.zsh
+++ fzf-collection.plugin.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-source "${0:h:A}/fzf-default.sh"
-command -v git >/dev/null && source "${0:h:A}/fzf-git.sh"
-command -v brew >/dev/null && source "${0:h:A}/fzf-brew.sh"
-command -v pip3 >/dev/null && source "${0:h:A}/fzf-pip.sh"
+for f in "${0:h:A}"/fzf-*.sh; do
+  source $f
+done
