496f1dc1dc9ba72dd16cc8e68ae76f9e4f43682f
liuyinz <cheunghsu@outlook.com>
2021-04-29 12:56:58 +0800

    style: add ref link

--- fzf-git.sh
+++ fzf-git.sh
@@ -74,6 +74,7 @@ geaf() {
 }
 
 # [G]it [S]ub[M]odule [R]emove
+#  FIXME @https://stackoverflow.com/questions/12641469/list-submodules-in-a-git-repository#comment84215697_12641787
 gsmr() {
   local inst
   inst=$(git config -z --file .gitmodules --get-regexp '\.path$' | \
