ba74b8032329b0690b0a708c533a59736eb32f57
liuyinz <cheunghsu@outlook.com>
2021-07-11 15:01:25 +0800

    fix: rename gch gcb

--- fzf-default.sh
+++ fzf-default.sh
@@ -2,25 +2,25 @@
 
 # Chorme
 # -----------------------
-# h - browse chrome history for MacOs
-h() {
+# gch - browse chrome history for MacOs
+gch() {
   local cols sep
   cols=$((COLUMNS / 3))
   sep='{::}'
 
-  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/gch
 
-  sqlite3 -separator $sep /tmp/h \
+  sqlite3 -separator $sep /tmp/gch \
     "select substr(title, 1, $cols), url
      from urls order by last_visit_time desc" |
     awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-    eval "fzf $FZF_COLLECTION_OPTS --ansi --header='[chrome: history]'" |
+    eval "fzf $FZF_COLLECTION_OPTS --ansi --header='[Google Chrome: history]'" |
     sed 's#.*\(https*://\)#\1#' |
     xargs open &>/dev/null
 }
 
-# b  - browse chrome Bookmarks for Macos
-b() {
+# gcb  - browse chrome Bookmarks for Macos
+gcb() {
   which jq >/dev/null 2>&1 || echo "jq is not installed !!!"
 
   local bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
@@ -32,7 +32,10 @@ join("/") } | .path + "/" + .name + "\t" + .url'
   jq -r "$jq_script" <"$bookmarks_path" |
     sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' |
     eval "fzf $FZF_COLLECTION_OPTS --ansi --no-hscroll --tiebreak=begin \
---header='[chrome: bookmark]'" | awk 'BEGIN { FS = "\t" } { print $2 }' |
+--header='[Google Chrome: bookmark]'" | awk 'BEGIN { FS = "\t" } { print $2 }' |
+    xargs open &>/dev/null
+}
+
     xargs open &>/dev/null
 }
 
