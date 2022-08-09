f993a16dc0dee9401f458915b7996e1894eaf0fe
liuyinz <liuyinz95@gmail.com>
2022-08-09 21:26:13 +0800

    fix(pipf): error when uninstall pip-autoremove

--- fzf-pip.sh
+++ fzf-pip.sh
@@ -37,7 +37,7 @@ _pipf_uninstall() {
   if [ "$1" = "pip" ]; then
     echo "Package [pip] couldn't be uninstalled !"
   else
-    if _fzf_exist pip-autoremove; then
+    if _fzf_exist pip-autoremove && [ "$1" != "pip-autoremove" ]; then
       # REQUIRE pip install pip-autoremove
       pip-autoremove "$1" --yes
     else
