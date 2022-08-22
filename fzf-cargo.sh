027b12fbc0862ea04fa99dd3265a9b9122d0622f
liuyinz <liuyinz95@gmail.com>
2022-08-23 03:34:15 +0800

    feat: add fzf-cargo

---
+++ fzf-cargo.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+_cargof_list_installed() {
+  cargo install --list \
+    | perl -lne '/^(.+)\sv(.+):$/ && print "$1 $2"'
+}
+
+_cargof_list_outdated() {
+  cargo install-update --list \
+    | perl -lne '/^(.+)\s+v(.+)\s+v(.+)\s+Yes$/ && print "$1 $2 $3"'
+}
+
+# TODO implemented
+# _cargof_list_avaiable() {}
+
+# SEE https://github.com/hcpl/crates.io-http-api-reference#user-content-get-versions
+_cargof_extract() {
+  local pkg field query
+
+  pkg="$1"
+  case $2 in
+    homepage)
+      field="$pkg"
+      query='.crate | .homepage // .repositorys // empty'
+      ;;
+    versions)
+      field="$pkg"
+      query='.versions[] | .num'
+      ;;
+    dependencies)
+      field="$pkg/$(_cargof_version_current)/dependencies"
+      query='.dependencies[] | "\(.crate_id) \(.req)"'
+      ;;
+    info)
+      field="$pkg"
+      query='.crate |"id: \(.id)\nmax_version: \(.max_version)\nrepository: \(.repository)\ndownloads: \(.downloads)\ncreated: \(.created_at)\nupdated: \(.updated_at)\ndescription: \(.description)"'
+      ;;
+    *)
+      echo "Error: No such option: $pkg" && return 0
+      ;;
+  esac
+
+  curl --silent "https://crates.io/api/v1/crates/$field" | jq -r "$query"
+}
+
+_cargof_version_current() {
+  cargo install --list 2>/dev/null \
+    | perl -slne '/^\Q$f\E\s+v(.+):$/ && print "$1"' -- -f="$pkg"
+}
+
+_cargof_version_install() {
+  echo "install $pkg: version $new"
+  cargo install --force --version "$new" -- "$pkg"
+}
+
+_cargof_rollback() {
+  local header pkg current install versions
+
+  pkg=$1
+  header=$(_fzf_header)
+  current="_cargof_version_current"
+  install="_cargof_version_install"
+  versions=$(_cargof_extract "$pkg" versions)
+
+  _fzf_rollback
+}
+
+_cargof_switch() {
+  local subcmd
+
+  subcmd=$(_fzf_subcmd)
+
+  if [ -n "$subcmd" ]; then
+
+    for f in $(echo "$inst"); do
+      case $subcmd in
+        update)
+          cargo install-update -- "$f" && _fzf_tmp_shift "$f"
+          ;;
+        uninstall)
+          cargo uninstall -- "$f" && _fzf_tmp_shift "$f"
+          ;;
+        install)
+          cargo install -- "$f" && _fzf_tmp_shift "$f"
+          ;;
+        rollback)
+          _cargof_rollback "$f"
+          ;;
+        homepage)
+          _fzf_homepage "$(_cargof_extract "$f" homepage)"
+          ;;
+        deps)
+          _cargof_extract "$f" dependencies | column -s ' ' -t
+          ;;
+        info)
+          _cargof_extract "$f" info
+          ;;
+        *)
+          cargo "$subcmd" "$f"
+          ;;
+      esac
+      echo ""
+    done
+
+    case $subcmd in
+      update | uninstall | rollback) return 0 ;;
+    esac
+
+  else
+    return 0
+  fi
+
+  _cargof_switch
+}
+
+cargof-outdated() {
+  local inst header opt tmpfile format outdated switch
+
+  header=$(_fzf_header)
+  tmpfile=$(_fzf_tmp_create)
+  format="outdated"
+  outdated="_cargof_list_outdated"
+  switch="_cargof_switch"
+  opt=("update" "uninstall" "rollback" "deps" "info")
+
+  _fzf_outdated
+}
+
+cargof-manage() {
+  local tmpfile inst header opt format installed switch
+
+  header=$(_fzf_header)
+  tmpfile=$(_fzf_tmp_create)
+  format="manage"
+  installed="_cargof_list_installed"
+  switch="_cargof_switch"
+  opt=("uninstall" "rollback" "homepage" "deps" "info")
+
+  _fzf_manage
+}
+
+# TODO search implements
+cargof-search() {
+  echo "Not implemented yet."
+}
+
+cargof() {
+  local header opt
+
+  header=$(_fzf_header)
+  opt=("outdated" "search" "manage")
+
+  _fzf_command
+}
--- fzf-collection.plugin.zsh
+++ fzf-collection.plugin.zsh
@@ -20,6 +20,7 @@ if [ -z "$FZF_COLLECTION_MODULES" ]; then
     brew
     npm
     pip
+    cargo
     proxy
     git
     gh
