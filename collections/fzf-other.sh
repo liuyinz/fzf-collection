065f4da12f451b87ef7556c98a0454ba13150574
liuyinz <liuyinz95@gmail.com>
2023-01-30 01:04:56 +0800

    fix(fp): recognize symbolic root_path too and add option =d

--- collections/fzf-other.sh
+++ collections/fzf-other.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 # [F]ind [P]ath
+# option -d return executable path
 
-# TODO add -d to echo dirpath, or full-filepath
 fp() {
-  local header format
+  local header format rule
   header="Find Path"
   format="general"
 
+  if [[ "$1" == "-d" ]]; then
+    rule='printf "%s/", glob($F[$#F])'
+  else
+    rule='printf "%s/%s", glob($F[$#F]), $F[0]'
+  fi
+
   for i in $(echo ${PATH//:/ }); do
     if [ -d "$i" ]; then
-      find "$i" -maxdepth 1 -executable -type f,l -printf "%f ${i/$HOME/~}\n"
+      # option -H to allow symblic root_path to parse normally
+      # SEE https://www.gnu.org/software/findutils/manual/html_node/find_html/Symbolic-Links.html
+      find -H "$i" -maxdepth 1 -executable -type f,l -printf "%f ${i/$HOME/~}\n"
     fi
   done \
     | _fzf_format \
     | uniq \
-    | _fzf_read --tiebreak=index
+    | fzf "${_fzf_opts[@]}" --header "$(_fzf_underline "$header")" --tiebreak=index \
+    | perl -lane "$rule"
 }
 
 # [F]ind [FP]ath
